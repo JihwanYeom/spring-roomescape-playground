8ece8bb6cc1a2dcaf474acdb9ae35d524826c6ed
jihwanyeom <yum020428@gmail.com>
2025-04-30 21:32:11 +0900

    test : 1단계 테스트

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,5 +1,7 @@
 package roomescape;
 
+import static org.hamcrest.Matchers.is;
+
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +9,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+
 public class MissionStepTest {
 
     @Test
@@ -16,4 +19,6 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200);
     }
+
+
 }
