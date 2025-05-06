208cbab4890bf09326e91d6cf12eb6a832285a23
jihwanyeom <yum020428@gmail.com>
2025-05-06 21:41:31 +0900

    feat - 예약 생성 및 삭제 기능 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,18 +1,24 @@
 package roomescape.controller;
 
 import jakarta.annotation.PostConstruct;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<Reservation>();
+    private final AtomicLong index = new AtomicLong(1);
 
     @PostConstruct
     public void initializeReservation() {
@@ -27,9 +33,20 @@ public class ReservationController {
     }
 
     @GetMapping("/reservations")
-    @ResponseBody
     public ResponseEntity<List<Reservation>> readReservations() {
         return ResponseEntity.ok().body(reservations);
     }
 
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
+        Reservation newReservation = new Reservation(index.incrementAndGet(), reservation);
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/members/" + newReservation.getId())).build();
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        reservations.remove(id);
+        return ResponseEntity.noContent().build();
+    }
 }
