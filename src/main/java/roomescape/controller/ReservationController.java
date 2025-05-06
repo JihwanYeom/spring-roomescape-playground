86768b7b2a223565f49d9e203d29d6ee61b7600b
jihwanyeom <yum020428@gmail.com>
2025-05-07 04:01:48 +0900

    feat - id를 통한 삭제 코드 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -5,27 +5,31 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.domain.Reservation;
 
-@RestController
+@Controller
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<Reservation>();
-    private final AtomicLong index = new AtomicLong(1);
+    private final AtomicLong index = new AtomicLong(0);
 
+    /*
     @PostConstruct
     public void initializeReservation() {
-        reservations.add(new Reservation(1L, "브라운", "2023-01-01", "10:00"));
-        reservations.add(new Reservation(2L, "브라운", "2023-01-02", "11:00"));
-        reservations.add(new Reservation(3L, "브라운", "2023-01-03", "12:00"));
+        reservations.add(new Reservation(index.incrementAndGet(), "브라운", "2023-01-01", "10:00"));
+        reservations.add(new Reservation(index.incrementAndGet(), "브라운", "2023-01-02", "11:00"));
+        reservations.add(new Reservation(index.incrementAndGet(), "브라운", "2023-01-03", "12:00"));
     }
+    */
 
     @GetMapping("/reservation")
     public String reservation() {
@@ -33,6 +37,7 @@ public class ReservationController {
     }
 
     @GetMapping("/reservations")
+    @ResponseBody
     public ResponseEntity<List<Reservation>> readReservations() {
         return ResponseEntity.ok().body(reservations);
     }
@@ -41,12 +46,13 @@ public class ReservationController {
     public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
         Reservation newReservation = new Reservation(index.incrementAndGet(), reservation);
         reservations.add(newReservation);
-        return ResponseEntity.created(URI.create("/members/" + newReservation.getId())).build();
+        System.out.println("newReservation.getId() = " + newReservation.getId());
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
-        reservations.remove(id);
+        reservations.removeIf(reservation -> reservation.getId().equals(id));
         return ResponseEntity.noContent().build();
     }
 }
