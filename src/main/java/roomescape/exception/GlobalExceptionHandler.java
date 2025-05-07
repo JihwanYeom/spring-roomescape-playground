b96a7a12234a0307a16a5d1808aaec3250c840e1
jihwanyeom <yum020428@gmail.com>
2025-05-07 20:25:47 +0900

    feat - 예외 및 전역 예외처리 클래스 추가

---
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package roomescape.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmptyDataException.class)
+    public ResponseEntity<Void> handleEmptyDataException(EmptyDataException e) {
+        System.out.println(e.getMessage());
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity<Void> handleNotFoundReservationException(NotFoundReservationException e) {
+        System.out.println(e.getMessage());
+        return ResponseEntity.badRequest().build();
+    }
+
+}
