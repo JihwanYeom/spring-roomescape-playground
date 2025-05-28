cf5df59f87729371bd5e33e9ee64d15961c9d5c7
jihwanyeom <yum020428@gmail.com>
2025-05-29 01:28:22 +0900

    style : 미사용 import 삭제

--- src/main/java/roomescape/dao/ReservationDao.java
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
-import roomescape.exception.NotFoundTimeIdException;
 
 @Repository
 public class ReservationDao {
